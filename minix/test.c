61034ff45f7471092c34f4c32ae58de78b8b8735
masami256 <masami256@gmail.com>
2010-05-01 09:34:45 +0900

    Added read_file().

--- minix/test.c
+++ minix/test.c
@@ -24,6 +24,9 @@ static void read_dentry(struct minix_dentry *dentry, unsigned long address, unsi
 static void read_inode(u_int16_t inode_num, struct minix_inode *inode, unsigned long addr);
 static void directory_walk(struct minix_superblock *sb, unsigned long address);
 static void find_file_test(struct minix_superblock *sb);
+static u_int16_t find_file(struct minix_superblock *sb, unsigned long address, const char *fname);
+static void read_file(struct minix_superblock *sb, const char *fname);
+static void read_file_test(struct minix_superblock *sb);
 
 #define get_first_data_zone(sb) (sb).s_firstdatazone * 0x400
 #define get_inode_table_address(sb) 0x800 + ((sb).s_imap_blocks * 0x400) + ((sb).s_zmap_blocks * 0x400)
@@ -129,6 +132,35 @@ static void directory_walk(struct minix_superblock *sb, unsigned long address)
 	}
 }
 
+static void read_file(struct minix_superblock *sb, const char *fname)
+{
+	u_int16_t ino;
+	struct minix_inode inode;
+	unsigned long inode_tbl_bass = get_inode_table_address(*sb);
+	char *data;
+	int i;
+
+	ino = find_file(sb, get_first_data_zone(*sb), fname);
+	if (!ino) {
+		printf("file %s not found\n", fname);
+		return ;
+	}
+
+	read_inode(ino, &inode, inode_tbl_bass);
+
+	printf("file %s: size is 0x%x\n", fname, inode.i_size);
+	data = malloc(inode.i_size);
+	assert(data != NULL);
+	memcpy(data, file_system + get_data_zone(inode.i_zone[0]),inode.i_size);
+
+	for (i = 0; i < inode.i_size; i++)
+		printf("0x%02x ", data[i]);
+
+	printf("\n");
+
+	
+}
+
 static int count_delimita_length(const char *str, char c)
 {
 	int i = 0;
@@ -212,16 +244,28 @@ int main(int argc, char **argv)
 	directory_walk(&sb, get_first_data_zone(sb));
 
 	find_file_test(&sb);
+	read_file_test(&sb);
 
 	munmap(file_system, size);
 
 	return 0;
 }
 
+static void read_file_test(struct minix_superblock *sb)
+{
+	printf(">>>>>>>>>>read_file() test <<<<<<<<<<<<<<<<\n");
+	read_file(sb, "/dir_a/dir_b/foobar.txt");
+	printf(">>>>>>>>>>read_file() test <<<<<<<<<<<<<<<<\n");
+	read_file(sb, "/test.txt");
+	printf(">>>>>>>>>>read_file() test <<<<<<<<<<<<<<<<\n");
+
+}
+
 static void find_file_test(struct minix_superblock *sb)
 {
 	u_int16_t ret;
 
+	printf(">>>>>>>>>>find_file() test <<<<<<<<<<<<<<<<\n");
 	ret = find_file(sb, get_first_data_zone(*sb), "/dir_a/dir_b/foobar.txt");
 	printf("file %s %s\n", "/dir_a/dir_b/foobar.txt", ret != 0 ? "Found" : "Not found");
 
@@ -230,13 +274,13 @@ static void find_file_test(struct minix_superblock *sb)
 
 	ret = find_file(sb, get_first_data_zone(*sb), "/dir_A/dir_B/");
 	printf("file %s %s\n", "/dir_A/dir_B/", ret != 0 ? "Found" : "Not found");
-
+	
 	ret = find_file(sb, get_first_data_zone(*sb), "/test.txt");
 	printf("file %s %s\n", "/test.txt", ret != 0 ? "Found" : "Not found");
 
 	ret = find_file(sb, get_first_data_zone(*sb), "/dir_A/dir_C");
 	printf("file %s %s\n", "/dir_A/dir_C", ret != 0 ? "Found" : "Not found");
-
+	printf(">>>>>>>>>>find_file() test <<<<<<<<<<<<<<<<\n");
 }
 
 
